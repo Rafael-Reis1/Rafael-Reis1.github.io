302d636e1af3cc49421771d4787cef8c1d213837
Rafael-Reis1 <rafaelreis.12@hotmail.com>
2025-02-16 03:30:56 -0300

    Melhorias

--- app.js
+++ app.js
@@ -3,30 +3,70 @@ window.onload = function() {
     const btnCurriculo = document.getElementById('btnCurriculo');
     const btnPortfolio = document.getElementById('btnPortfolio');
     const sobre = document.getElementById('sobre');
+    const curriculo = document.getElementById('curriculo');
     const github = document.getElementById('github');
     const instagram = document.getElementById('instagram');
     const linkedin = document.getElementById('linkedin');
     const mail = document.getElementById('mail');
+    const mainInfo = document.querySelector('.mainInfo');
+    const portfolio = document.getElementById('portfolio');
+    mainInfo.style.height = sobre.offsetHeight + 'px';
 
     btnSobre.onclick = function() {
+        sobre.style.display = 'block';
+        setTimeout(function() {
+            sobre.style.opacity = 1;
+        }, 100);
+
+        mainInfo.style.height = sobre.offsetHeight + 'px';
         btnSobre.classList.add('btnAccentColor');
         btnCurriculo.classList.remove('btnAccentColor');
         btnPortfolio.classList.remove('btnAccentColor');
-        sobre.style.opacity = 1;
+        curriculo.style.opacity = 0;
+        portfolio.style.opacity = 0;
+        
+        setTimeout(function() {
+            curriculo.style.display = 'none';
+            portfolio.style.display = 'none';
+        }, 250); 
     }
 
     btnCurriculo.onclick = function() {
+        curriculo.style.display = 'block';
+        setTimeout(function() {
+            curriculo.style.opacity = 1;
+        }, 100);
+
+        mainInfo.style.height = curriculo.offsetHeight + 'px';
         btnSobre.classList.remove('btnAccentColor');
         btnCurriculo.classList.add('btnAccentColor');
         btnPortfolio.classList.remove('btnAccentColor');
         sobre.style.opacity = 0;
+        portfolio.style.opacity = 0;
+        
+        setTimeout(function() {
+            sobre.style.display = 'none';
+            portfolio.style.display = 'none';
+        }, 250); 
     }
 
     btnPortfolio.onclick = function() {
+        portfolio.style.display = 'block';
+        setTimeout(function() {
+            portfolio.style.opacity = 1;
+        }, 100);
+
+        mainInfo.style.height = portfolio.offsetHeight + 'px';
         btnSobre.classList.remove('btnAccentColor');
         btnCurriculo.classList.remove('btnAccentColor');
         btnPortfolio.classList.add('btnAccentColor');
         sobre.style.opacity = 0;
+        curriculo.style.opacity = 0;
+
+        setTimeout(function() {
+            curriculo.style.display = 'none';
+            sobre.style.display = 'none';
+        }, 250);
     }
 
     github.onclick = function() {
