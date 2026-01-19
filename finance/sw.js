93cf7f6fe2079a5689f8ae1bf76baf27f46e8f49
Rafael-Reis1 <reisr5941@gmail.com>
2026-01-19 02:42:19 -0300

    Updates!

--- finance/sw.js
+++ finance/sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'finance-app-?v=1901260213';
+const CACHE_NAME = 'finance-app-?v=1901260240';
 const urlsToCache = [
     '/finance/finance.html',
-    '/finance/style.css?v=1901260213',
-    '/finance/app.js?v=1901260213',
+    '/finance/style.css?v=1901260240',
+    '/finance/app.js?v=1901260240',
     '/finance/icon-512.png',
     '/finance/icon-maskable.png',
-    '/finance/manifest.json?v=1901260213',
+    '/finance/manifest.json?v=1901260240',
     '../assets/libs_comuns/firebase/10.7.1/firebase-app-compat.js',
     '../assets/libs_comuns/firebase/10.7.1/firebase-auth-compat.js',
     '../assets/libs_comuns/firebase/10.7.1/firebase-firestore-compat.js',
--- pdfFormatter/sw.js
+++ pdfFormatter/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'pdf-booklet-?v=1901260213';
+const CACHE_NAME = 'pdf-booklet-?v=1901260240';
 const urlsToCache = [
     './pdfFormater.html',
-    './style.css?v=1901260213',
-    './app.js?v=1901260213',
-    '../style.css?v=1901260213',
-    './manifest.json?v=1901260213',
+    './style.css?v=1901260240',
+    './app.js?v=1901260240',
+    '../style.css?v=1901260240',
+    './manifest.json?v=1901260240',
     '/imgs/arrow_back_white.webp',
     '../Leitor-logs-totvs-fluig/assets/upload.webp',
     '../Leitor-logs-totvs-fluig/assets/upload_blue.webp',
