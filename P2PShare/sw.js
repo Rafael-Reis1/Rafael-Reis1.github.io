c1851331dcbc6beb7bd57f2e8b519056ac78b6f4
Rafael Reis da Silveira <reisr5941@gmail.com>
2026-07-25 11:49:32 -0300

    Updates!

--- P2PShare/sw.js
+++ P2PShare/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'p2pshare-?v=2507261137';
+const CACHE_NAME = 'p2pshare-?v=2507261149';
 const urlsToCache = [
     './P2PShare.html',
-    './style.css?v=2507261137',
-    './app.js?v=2507261137',
-    './manifest.json?v=2507261137',
+    './style.css?v=2507261149',
+    './app.js?v=2507261149',
+    './manifest.json?v=2507261149',
     './assets/icon-512.png',
     '/imgs/arrow_back_white.webp',
     '../Leitor-logs-totvs-fluig/assets/upload.webp',
--- pdfFormatter/sw.js
+++ pdfFormatter/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'pdf-booklet-?v=2507261137';
+const CACHE_NAME = 'pdf-booklet-?v=2507261149';
 const urlsToCache = [
     './pdfFormater.html',
-    './style.css?v=2507261137',
-    './app.js?v=2507261137',
-    '../style.css?v=2507261137',
-    './manifest.json?v=2507261137',
+    './style.css?v=2507261149',
+    './app.js?v=2507261149',
+    '../style.css?v=2507261149',
+    './manifest.json?v=2507261149',
     '/imgs/arrow_back_white.webp',
     '../Leitor-logs-totvs-fluig/assets/upload.webp',
     '../Leitor-logs-totvs-fluig/assets/upload_blue.webp',
